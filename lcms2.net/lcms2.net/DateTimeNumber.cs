185fb73f5021d4abbef9019aa577fe8f3e9c1ec9
Stefan Kewatt <stefan.kewatt@gmail.com>
2021-07-15 15:55:13 -0500

    Add ICCData and DataTimeNumber structs.

---
+++ lcms2.net/lcms2.net/DateTimeNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lcms2dotnet
+{
+    public struct DateTimeNumber
+    {
+        public ushort Year;
+        public ushort Month;
+        public ushort Day;
+        public ushort Hours;
+        public ushort Minutes;
+        public ushort Seconds;
+    }
+}
---
+++ lcms2.net/lcms2.net/ICCData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lcms2dotnet
+{
+    public struct ICCData
+    {
+        public uint Length;
+        public uint Flags;
+        public Memory<byte> Data;
+    }
+}
