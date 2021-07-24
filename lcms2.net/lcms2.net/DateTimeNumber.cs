7677b474bbfd23fc9363c20b7dd29e549512ca62
Stefan Kewatt <stefan.kewatt@gmail.com>
2021-07-24 10:26:08 -0500

    Added casting to and from DateTime and DateTimeNumber

--- lcms2.net/lcms2.net/DateTimeNumber.cs
+++ lcms2.net/lcms2.net/DateTimeNumber.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,5 +14,22 @@ namespace lcms2dotnet
         public ushort Hours;
         public ushort Minutes;
         public ushort Seconds;
+
+        public static explicit operator DateTimeNumber(DateTime dateTime) =>
+            new() { 
+                Year =  (ushort)dateTime.Year, 
+                Month = (ushort)dateTime.Month, 
+                Day = (ushort)dateTime.Day, 
+                Hours = (ushort)dateTime.Hour, 
+                Minutes = (ushort)dateTime.Minute, 
+                Seconds = (ushort)dateTime.Second };
+
+        public static explicit operator DateTime(DateTimeNumber dateTime) =>
+            new(dateTime.Year,
+                dateTime.Month,
+                dateTime.Day,
+                dateTime.Hours,
+                dateTime.Minutes,
+                dateTime.Seconds);
     }
 }
