1176073153a838b8efa04ee356d8224f7cf8388f
Stefan Kewatt <stefan.kewatt@gmail.com>
2021-07-24 10:27:28 -0500

    Converted ErrorType from a enum to a static class with readonly fields.

--- lcms2.net/lcms2.net/ErrorType.cs
+++ lcms2.net/lcms2.net/ErrorType.cs
@@ -1,20 +1,22 @@
-﻿namespace lcms2dotnet
+﻿using Microsoft.Extensions.Logging;
+
+namespace lcms2dotnet
 {
-    public enum ErrorType
+    public static class ErrorType
     {
-        Undefined = 0,
-        File = 1,
-        Range = 2,
-        Internal = 3,
-        Null = 4,
-        Read = 5,
-        Seek = 6,
-        Write = 7,
-        UnknownExtension = 8,
-        ColorSpaceCheck = 9,
-        AlreadyDefined = 10,
-        BadSignature = 11,
-        CorruptionDetected = 12,
-        NotSuitable = 13,
+        public static readonly EventId Undefined = new(0);
+        public static readonly EventId File = new(1);
+        public static readonly EventId Range = new(2);
+        public static readonly EventId Internal = new(3);
+        public static readonly EventId Null = new(4);
+        public static readonly EventId Read = new(5);
+        public static readonly EventId Seek = new(6);
+        public static readonly EventId Write = new(7);
+        public static readonly EventId UnknownExtension = new(8);
+        public static readonly EventId ColorSpaceCheck = new(9);
+        public static readonly EventId AlreadyDefined = new(10);
+        public static readonly EventId BadSignature = new(11);
+        public static readonly EventId CorruptionDetected = new(12);
+        public static readonly EventId NotSuitable = new(13);
     }
 }
