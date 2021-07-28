4591a60a4da973d16fd2a04cf86ffcf7ae7f5f39
Stefan Kewatt <stefan.kewatt@gmail.com>
2021-07-27 21:20:11 -0500

    Added SizeOf class to hold the expected size of structs.

---
+++ lcms2.net/lcms2.net/SizeOf.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lcms2dotnet
+{
+    internal static class SizeOf
+    {
+        public const int Signature = sizeof(uint);
+        public const int TagInfo = (sizeof(uint) * 2) + Signature;
+    }
+}
