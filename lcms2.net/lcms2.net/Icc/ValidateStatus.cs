32ba99d83bdc4611a81c5a5e3fcb61864a39c22d
Stefan Kewatt <stefan.kewatt@gmail.com>
2021-07-27 21:29:38 -0500

    Add ValidateStatus return code enum

---
+++ lcms2.net/lcms2.net/Icc/ValidateStatus.cs
@@ -0,0 +1,25 @@
+﻿namespace lcms2dotnet.Icc
+{
+    /// <summary>
+    /// Validation Status values
+    /// </summary>
+    public enum ValidateStatus
+    {
+        /// <summary>
+        /// Profile is valid and conforms to specification
+        /// </summary>
+        Ok,
+        /// <summary>
+        /// Profile conforms to specification with concerns
+        /// </summary>
+        Warning,
+        /// <summary>
+        /// Profile does not conform to specification, but may still be useable
+        /// </summary>
+        NonCompliant,
+        /// <summary>
+        /// Profile does not conform to specification and is not usable
+        /// </summary>
+        CriticalError,
+    }
+}
